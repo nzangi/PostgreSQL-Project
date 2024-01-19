1cc3bc3d1b754db30722d36e0ab3b5577a98c450
nzangi <nzangimuoki284@gmail.com>
2024-01-20 00:16:36 +0300

    Full PostgreSQL-Project

--- link_db.py
+++ link_db.py
@@ -3,7 +3,9 @@ from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 password = "Enter_Your_PostgreSQL_Here"
 encoded_password = quote_plus(password)
-conn_string= f'postgresql://nzangi:{encoded_password}@localhost/sqlpostgres'
+postgreSQL_username = "Your_postgresql_useranme"
+db_name = 'Your_DB_Name'
+conn_string= f'postgresql://{postgreSQL_username}:{encoded_password}@localhost/{db_name}'
 db = create_engine(conn_string)
 conn = db.connect()
 files =['artist','canvas_size','image_link','museum','museum_hours','product_size','subject','work']
