2f24200fd0a6018f353ab28889760f4c99ba6a50
nzangi <nzangimuoki284@gmail.com>
2024-01-31 17:06:20 +0300

    working on sql commands

--- sqlquestions.session.sql
+++ sqlquestions.session.sql
@@ -74,3 +74,79 @@ SELECT * FROM(
 SELECT *,row_number() 
 OVER(ORDER BY emp_id DESC) as rn FROM employee)
 x WHERE rn=2;
+
+--@block
+create table employee
+( emp_ID int primary key
+, emp_NAME varchar(50) not null
+, DEPT_NAME varchar(50)
+, SALARY int);
+
+insert into employee values(101, 'Mohan', 'Admin', 4000);
+insert into employee values(102, 'Rajkumar', 'HR', 3000);
+insert into employee values(103, 'Akbar', 'IT', 4000);
+insert into employee values(104, 'Dorvin', 'Finance', 6500);
+insert into employee values(105, 'Rohit', 'HR', 3000);
+insert into employee values(106, 'Rajesh',  'Finance', 5000);
+insert into employee values(107, 'Preet', 'HR', 7000);
+insert into employee values(108, 'Maryam', 'Admin', 4000);
+insert into employee values(109, 'Sanjay', 'IT', 6500);
+insert into employee values(110, 'Vasudha', 'IT', 7000);
+insert into employee values(111, 'Melinda', 'IT', 8000);
+insert into employee values(112, 'Komal', 'IT', 10000);
+insert into employee values(113, 'Gautham', 'Admin', 2000);
+insert into employee values(114, 'Manisha', 'HR', 3000);
+insert into employee values(115, 'Chandni', 'IT', 4500);
+insert into employee values(116, 'Satya', 'Finance', 6500);
+insert into employee values(117, 'Adarsh', 'HR', 3500);
+insert into employee values(118, 'Tejaswi', 'Finance', 5500);
+insert into employee values(119, 'Cory', 'HR', 8000);
+insert into employee values(120, 'Monica', 'Admin', 5000);
+insert into employee values(121, 'Rosalin', 'IT', 6000);
+insert into employee values(122, 'Ibrahim', 'IT', 8000);
+insert into employee values(123, 'Vikram', 'IT', 8000);
+insert into employee values(124, 'Dheeraj', 'IT', 11000);
+
+--@block
+SELECT e.emp_ID,e.emp_NAME,e.DEPT_NAME,e.salary,max_salary.max_salary 
+FROM employee as e INNER JOIN(
+    SELECT DEPT_NAME, max(salary) as max_salary FROM employee  GROUP BY DEPT_NAME) 
+    as max_salary 
+    ON e.salary = max_salary.max_salary AND
+    e.DEPT_NAME = max_salary.DEPT_NAME
+
+UNION
+
+SELECT e.emp_ID,e.emp_NAME,e.DEPT_NAME,e.salary,min_salary.min_salary 
+FROM employee as e INNER JOIN(
+    SELECT DEPT_NAME, min(salary) as min_salary FROM employee  GROUP BY DEPT_NAME) 
+    as min_salary 
+    ON e.salary = min_salary.min_salary AND
+    e.DEPT_NAME = min_salary.DEPT_NAME
+
+ORDER BY DEPT_NAME ASC
+;
+--@block
+
+SELECT e.emp_ID,e.emp_NAME, e.DEPT_NAME,e.salary,max_salary.max_salary,min_salary.min_salary
+FROM employee as e 
+
+JOIN(
+    SELECT DISTINCT DEPT_NAME, max(salary) as max_salary FROM employee  GROUP BY DEPT_NAME) 
+    as max_salary 
+    ON
+    e.DEPT_NAME = max_salary.DEPT_NAME
+    AND e.salary = max_salary.max_salary
+
+JOIN(
+    SELECT  DISTINCT DEPT_NAME, min(salary) as min_salary FROM employee  GROUP BY DEPT_NAME) 
+    as min_salary 
+    ON
+    e.DEPT_NAME = min_salary.DEPT_NAME
+    AND e.salary = min_salary.min_salary
+
+ORDER BY DEPT_NAME ASC
+;
+
+--@block
+SELECT * FROM employee;
